66e9b8ea8f0b16d83446b099f299c53244018d34
zahralatif <zahralatif2107@gmail.com>
2023-07-08 10:15:18 +0400

    firstcommit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # console_app.csharp
+
+1. Simple Creature
+![Simple Creature](simplecreature.png)
---
+++ simple_creature.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace firstprogram060723
+{
+    class humanrobot
+    {
+        static void Main(string[] args)
+        {
+            /* my first program in C# */
+            Console.WriteLine(" .-."); 
+            Console.WriteLine("(o o)");
+            Console.WriteLine("| 0 |");
+            Console.WriteLine("|   |");
+            Console.WriteLine("'~~~'");
+
+            Console.ReadKey();
+        }
+    }
+}
