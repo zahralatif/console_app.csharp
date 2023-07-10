3cdb28179d70bc08507b6d70da265d5d761da0f5
zahralatif <zahralatif2107@gmail.com>
2023-07-10 19:42:54 +0400

    holmescommit

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 1. Simple Creature
 ![Simple Creature](simplecreature.png)
+
+2. Holmes Creature
+![Holmes Creature](holmescreature.png)
---
+++ holmes_creature.cs
@@ -0,0 +1,30 @@
+using System;
+
+namespace firstprogram060723
+{
+    class humanrobot
+    {
+        static void Main(string[] args)
+        {
+            /* my first program in C# */
+            Console.WriteLine("c_");
+            Console.WriteLine("      -");
+            Console.WriteLine("  \\\\_|_|_");
+            Console.WriteLine("    \\( o_o)");
+            Console.WriteLine("     > ~  >");
+            Console.WriteLine("    /    / \\\\");
+            Console.WriteLine("   /    /   \\\\");
+            Console.WriteLine("   \\    )   c/");
+            Console.WriteLine("   /   /");
+            Console.WriteLine("  /  /");
+            Console.WriteLine(" (  ( \\");
+            Console.WriteLine(" |  |  \\");
+            Console.WriteLine(" |  |   \\");
+            Console.WriteLine(" | / \\  )");
+            Console.WriteLine(" | |  ) |");
+            Console.WriteLine(" / )(_/");
+            Console.WriteLine("(_ /");
+            Console.ReadKey();
+        }
+    }
+}
