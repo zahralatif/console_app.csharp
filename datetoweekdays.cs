bc54ba021512bddde89b47d4b381b0a1b88add98
zahralatif <zahralatif2107@gmail.com>
2023-07-29 20:03:14 +0400

    16thcommit

--- datetoweekdays.cs
+++ datetoweekdays.cs
@@ -1,15 +1,18 @@
 using System;
 
-class Program
+namespace datetoweekdays
 {
-    static void Main(string[] args)
+    class Program
     {
-        Console.Write("Enter a date (dd-mm-yyyy): ");
-        string input = Console.ReadLine();
-        DateTime date = DateTime.ParseExact(input, "dd-MM-yyyy", null);
-        string weekday = date.ToString("dddd");
-        Console.WriteLine($"The weekday of {input} is {weekday}");
-        Console.ReadKey();
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a date (dd-mm-yyyy): ");
+            string input = Console.ReadLine();
+            DateTime date = DateTime.ParseExact(input, "dd-MM-yyyy", null);
+            string weekday = date.ToString("dddd");
+            Console.WriteLine($"The weekday of {input} is {weekday}");
+            Console.ReadKey();
 
+        }
     }
 }
